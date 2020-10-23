ee1624ff78e1d7b57e382e05656a3121a510b64d
crazythinking <crazy_thinking@163.com>
2020-10-23 12:14:58 +0800

    add busstream starter based on spring cloud bus and stream

---
+++ bustream-spring-boot-starter/src/main/java/net/engining/bustream/base/GenericRemoteApplicationEvent.java
@@ -0,0 +1,29 @@
+package net.engining.bustream.base;
+
+import org.springframework.cloud.bus.event.RemoteApplicationEvent;
+
+/**
+ * {@link RemoteApplicationEvent} for Supporting Generic
+ *
+ * @author Eric Lu
+ */
+public class GenericRemoteApplicationEvent<T> extends RemoteApplicationEvent {
+
+	private T target;
+
+	public GenericRemoteApplicationEvent() {
+	}
+
+	public GenericRemoteApplicationEvent(Object source, T target, String originService, String destinationService) {
+		super(source, originService, destinationService);
+		this.target = target;
+	}
+
+	public T getTarget() {
+		return target;
+	}
+
+	public void setTarget(T target) {
+		this.target = target;
+	}
+}
