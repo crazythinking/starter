51981fc34c8f4c9d7a62bf3aa10b0a03df3b5566
crazythinking <crazy_thinking@163.com>
2021-02-19 18:06:28 +0800

    添加分布式锁支持

---
+++ distlock-spring-boot-starter/src/test/java/net/engining/lock/autoconfigure/autotest/support/User.java
@@ -0,0 +1,42 @@
+package net.engining.lock.autoconfigure.autotest.support;
+
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
