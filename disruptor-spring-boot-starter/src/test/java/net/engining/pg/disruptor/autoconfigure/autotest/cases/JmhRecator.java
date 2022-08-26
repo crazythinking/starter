5995be257265082a674fa12191c71b938e45cabb
crazythinking <crazy_thinking@163.com>
2022-08-26 09:36:49 +0800

    Recator测试

---
+++ disruptor-spring-boot-starter/src/test/java/net/engining/pg/disruptor/autoconfigure/autotest/cases/JmhRecator.java
@@ -0,0 +1,37 @@
+package net.engining.pg.disruptor.autoconfigure.autotest.cases;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : Eric Lu
+ * @version :
+ * @date : 2022-08-24 15:53
+ * @since :
+ **/
+@BenchmarkMode(Mode.AverageTime) // 测试方法平均执行时间
+@OutputTimeUnit(TimeUnit.MICROSECONDS) // 输出结果的时间粒度为微秒
+@State(Scope.Thread) // 每个测试线程一个实例
+public class JmhRecator {
+
+    public static void main(String[] args) throws RunnerException {
+        // 使用一个单独进程执行测试，执行5遍warmup，然后执行5遍测试
+        Options opt = new OptionsBuilder()
+                //指定Benchmark的类，执行其内添加注解@Benchmark的方法
+                .include(ReactorTest.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .build();
+        new Runner(opt).run();
+    }
+}
--- disruptor-spring-boot-starter/src/test/java/net/engining/pg/disruptor/autoconfigure/autotest/cases/ReactorTest.java
+++ disruptor-spring-boot-starter/src/test/java/net/engining/pg/disruptor/autoconfigure/autotest/cases/ReactorTest.java
@@ -2,25 +2,32 @@ package net.engining.pg.disruptor.autoconfigure.autotest.cases;
 
 import cn.hutool.core.lang.Console;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
 
 /**
+ * 根据Benchmark的测试报告，BaseSubscriber#request(n)，n的大小与性能正相关；
+ *
  * @author : Eric Lu
  * @version :
  * @date : 2021-03-07 20:23
  * @since :
  **/
-public class RecatorTest {
+public class ReactorTest {
 
     @Test
+    @Benchmark
     public void range() {
         // [1] 构建一个发布者Flux; 注意，这时发布者还没开始生产数据。
-        Flux<Integer> flux = Flux.range(1, 10);
+        Flux<Integer> flux = Flux.range(1, 20);
         // [2] 构建一个订阅者Subscriber
         Subscriber<Integer> subscriber = new BaseSubscriber<Integer>() {
             protected void hookOnNext(Integer value) {
@@ -34,8 +41,9 @@ public class RecatorTest {
     }
 
     @Test
+    @Benchmark
     public void range2() {
-        Flux<Integer> flux = Flux.range(1, 100);
+        Flux<Integer> flux = Flux.range(1, 20);
         // [2] 构建一个订阅者Subscriber
         Subscriber<Integer> subscriber = new BaseSubscriber<Integer>() {
             protected void hookOnNext(Integer value) {
@@ -72,6 +80,41 @@ public class RecatorTest {
                 .publishOn(Schedulers.newParallel("myParallel"))
                 .skip(1)
                 .subscribe(myHandler);
+    }
+
+    @Test
+    public void backpressure() throws InterruptedException {
+        //1
+        Flux.<Integer>create(sink -> {
+                    for (int i = 0; i < 50; i++) {
+                        System.out.println("push: " + i);
+                        sink.next(i);
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }, FluxSink.OverflowStrategy.ERROR)
+                //2
+                .publishOn(Schedulers.newSingle("receiver"), 10)
+                .subscribe(new BaseSubscriber<Integer>() {
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        subscription.request(1);
+                    }
+                    protected void hookOnNext(Integer value) {
+                        System.out.println("receive:" + value);
+                        try {
+                            Thread.sleep(20);
+                        } catch (InterruptedException e) {
+                        }
+                        request(1);
+                    }
+                    protected void hookOnError(Throwable throwable) {
+                        throwable.printStackTrace();
+                        System.exit(1);
+                    }
+                });
+        //new CountDownLatch(1).await();
     }
 
 }
